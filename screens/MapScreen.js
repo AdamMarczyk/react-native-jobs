4872de8fb5ad50ba96e0d65871cc28de8b22a50e
Adam Marczyk <adam.marczyk90@gmail.com>
2018-08-09 19:38:06 +0200

    fetch data

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { View, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
+import { connect } from 'react-redux';
+import { Button } from 'react-native-elements';
+import * as actions from '../actions';
 
 class MapScreen extends Component {
   state = {
@@ -17,6 +20,14 @@ class MapScreen extends Component {
     this.setState({ mapLoaded: true });
   }
 
+  onRegionChangeComplete = region => {
+    this.setState({ region });
+  };
+
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region);
+  };
+
   render() {
     if (!this.state.mapLoaded) {
       return (
@@ -27,10 +38,35 @@ class MapScreen extends Component {
     }
     return (
       <View style={{ flex: 1 }}>
-        <MapView region={this.state.region} style={{ flex: 1 }} />
+        <MapView
+          region={this.state.region}
+          style={{ flex: 1 }}
+          onRegionChangeComplete={this.onRegionChangeComplete}
+        />
+        <View style={styles.buttonContainer}>
+          <Button
+            large
+            title="Search this area"
+            backgroundColor="#009688"
+            icon={{ name: 'search' }}
+            onPress={this.onButtonPress}
+          />
+        </View>
       </View>
     );
   }
 }
 
-export default MapScreen;
+const styles = {
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0
+  }
+};
+
+export default connect(
+  null,
+  actions
+)(MapScreen);
